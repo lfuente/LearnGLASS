3aa4a3f5930dd51349a65ee43fe0e219bbd0e723
Iago <iagoqo@inv.it.uc3m.es>
2012-10-01 12:48:08 +0200

    Fixed problems in cron's mapreduce, still, there is redundant code and it can be further improved

--- capture/Cron/mapreduce.php
+++ capture/Cron/mapreduce.php
@@ -8,47 +8,78 @@
 	$name = "test1";
 	$host = "localhost";
 
-	$database = MongoConnect($user,$pass,$database,$host);
+	$database = MongoConnect($user,$pass,$name,$host);
 	//$connection = new Mongo("mongodb://$user:$pass@$host/$name");
 	MongoCursor::$timeout = -1; // if the process is too long
 	
-	$database = $connection->selectDB("$name");
+	//$database = $connection->selectDB("$name");
 	
 	$collections = $database->listCollections();
 	
 	foreach ($collections as $collection) {
-		$name = $collection->getName();
-		if(strstr($name,'role')){
-			$name = substr($name,4);
-			echo "$name\n";
+		$colname = $collection->getName();
+		if(strstr($colname,'role')){
+			$colname = substr($colname,4);
+			echo "$colname\n";
+			
+			if($colname=="user"){
+				//Users MAP-REDUCE
+				$mapString = '
+					function() {
+						var date = new Date(this.datetime);
+						dateString = (date.getYear()+1900)+"-"+(date.getMonth()+1)+"-"+date.getDate();
+						for (var user in this.user) {
+							emit({name:this.user[user]._id,date:dateString,type:this.name},{count:1});
+						}
+					}
+				';
+				$reduceString = '
+					function(k,v){
+						var sum = 0;
+						for (var i=0;i<v.length;i++){
+							sum += v[i].count; ;            
+						}
+						return {count:sum};
+					}
+				';
+				$mapCode = new MongoCode($mapString);
+				$reduceCode = new MongoCode($reduceString);
+				$database->command(array(
+					'mapreduce' => 'events',
+					'map' => $mapCode,
+					'reduce' => $reduceCode,
+					'out' => "roleuser"
+				));
+			}
+			else {
+				$mapString = '
+					function() {
+						var date = new Date(this.datetime);
+						dateString = (date.getYear()+1900)+"-"+(date.getMonth()+1)+"-"+date.getDate();
+						if(this.'.$colname.'){
+							emit({name:this.'.$colname.',date:dateString,type:this.name},{count:1});
+						}
+					}
+				';
+				$reduceString = '
+					function(k,v){
+						var sum = 0;
+						for (var i=0;i<v.length;i++){
+							sum += v[i].count; ;            
+						}
+						return {count:sum};
+					}
+				';
+				$mapCode = new MongoCode($mapString);
+				$reduceCode = new MongoCode($reduceString);
+				$database->command(array(
+					'mapreduce' => 'events',
+					'map' => $mapCode,
+					'reduce' => $reduceCode,
+					'out' => "role".$colname
+				)); 
+			}
 		}
 	}
-	
-	//~ $strMap = '
-		//~ function(){
-			//~ var date = new Date(this.datetime);
-			//~ dateString = (date.getYear()+1900)+"-"+(date.getMonth()+1)+"-"+date.getDate();
-			//~ for (var user in this.user) {
-				//~ emit({name:this.user[user]._id,date:dateString,type:this.name},{count:1});
-			//~ }
-		//~ }
-	//~ ';
-	//~ $strReduce = '
-		//~ function(k,v){
-			//~ var sum = 0;
-			//~ for (var i=0;i<v.length;i++){
-				//~ sum += v[i].count;
-			//~ }
-			//~ return {count:sum};
-		//~ }
-	//~ ';
-	//~ $mapCode = new MongoCode($strMap);
-	//~ $reduceCode = new MongoCode($strReduce);
-	//~ $database->command(array(
-		//~ 'mapreduce' => 'events',
-		//~ 'map' => $mapCode,
-		//~ 'reduce' => $reduceCode,
-		//~ 'out' => "roleuser"
-	//~ ));
 	
 ?>
